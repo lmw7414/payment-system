15edb256cf6f2de72c600d9e8c318ec7e557bca2
Lee Min Woo <lmw7414@naver.com>
2025-02-27 18:09:24 +0900

    동시 요청상태에서 테스트 확인
    결제나 충전 로직에 동시에 요청하는 경우에 대해 테스트를 진행함.
    비동기 처리에 대해서는 무결성을 보장하지 못함.
    
    *todo
    네트워크 문제에서 발생할 수 있는 경우
    - read timeout
    - connection timeout
    -> 일단은 클라이언트에서 보내주는 멱등키를 사용하여 어느정도 예방은 한 상태이지만, 네트워크 상태의 에러가 아닌 경우도 생각해봐야 함.

--- src/test/java/org/example/paymentsystem/TransactionServiceIntegrationTest.java
+++ src/test/java/org/example/paymentsystem/TransactionServiceIntegrationTest.java
@@ -9,10 +9,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class TransactionServiceIntegrationTest {
 
     @Autowired
@@ -26,8 +32,8 @@ public class TransactionServiceIntegrationTest {
 
     @BeforeEach
     void setup() {
-        walletService.createWallet(new CreateWalletRequest(1L));
-        walletService.addBalance(new AddBalanceWalletRequest(1L, BigDecimal.valueOf(10000)));
+        CreateWalletResponse wallet = walletService.createWallet(new CreateWalletRequest(1L));
+        walletService.addBalance(new AddBalanceWalletRequest(wallet.id(), BigDecimal.valueOf(10000)));
     }
 
     @AfterEach
@@ -40,13 +46,13 @@ public class TransactionServiceIntegrationTest {
     @Transactional
     public void 결제를_생성한다() {
         // Given
-        PaymentTransactionRequest request = new PaymentTransactionRequest(1L, "course-1", new BigDecimal(10000));
+        FindWalletResponse wallet = walletService.findWalletByUserId(1L);
+        PaymentTransactionRequest request = new PaymentTransactionRequest(wallet.id(), "course-1", new BigDecimal(10000));
         // When
         PaymentTransactionResponse response = transactionService.payment(request);
         // Then
         Assertions.assertNotNull(response);
         System.out.println(response);
-
     }
 
     @Test
@@ -59,6 +65,30 @@ public class TransactionServiceIntegrationTest {
         // Then
         Assertions.assertNotNull(response);
         System.out.println(response);
+    }
 
+    @Test
+    public void 충전을_동시에_실행하는_경우() throws InterruptedException {
+        int numOfThread = 20;
+        System.out.println("사용자 지갑 정보" + walletRepository.findAll());
+        ChargeTransactionRequest request = new ChargeTransactionRequest(1L, "order-1", BigDecimal.valueOf(1000));
+        ExecutorService service = Executors.newFixedThreadPool(numOfThread);
+        AtomicInteger completeTasks = new AtomicInteger(0);
+
+        for (int i = 0; i < numOfThread; i++) {
+            service.submit(() -> {
+                try {
+                    transactionService.charge(request);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    completeTasks.incrementAndGet();
+                }
+            });
+        }
+        service.shutdown();
+        boolean finished = service.awaitTermination(1, TimeUnit.MINUTES);
+        System.out.println(finished);
+        System.out.println(walletRepository.findWalletByUserId(1L));
     }
 }
--- src/test/java/org/example/paymentsystem/WalletServiceIntegrationTest.java
+++ src/test/java/org/example/paymentsystem/WalletServiceIntegrationTest.java
@@ -7,8 +7,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class WalletServiceIntegrationTest {
 
     @Autowired
@@ -23,6 +30,29 @@ public class WalletServiceIntegrationTest {
         // Then
         Assertions.assertNotNull(response);
         System.out.println(response);
+    }
 
+    @Test
+    public void 동시에_여러건의_계좌가_생성된다면() throws InterruptedException {
+        Long userId = 2L;
+        CreateWalletRequest request = new CreateWalletRequest(userId);
+        int numOfThread = 20;
+        ExecutorService service = Executors.newFixedThreadPool(numOfThread);
+        AtomicInteger completeTasks = new AtomicInteger(0);
+
+        for (int i = 0; i < numOfThread; i++) {
+            service.submit(() -> {
+                try {
+                    walletService.createWallet(request);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    completeTasks.incrementAndGet();
+                }
+            });
+        }
+        service.shutdown();
+        boolean finished = service.awaitTermination(1, TimeUnit.MINUTES);
+        System.out.println(finished);
     }
 }
